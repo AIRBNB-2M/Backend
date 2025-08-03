7306f0af84a75a4b381733b0cf2c659d7e5528a2
이동진 <ehdwls010203@gmail.com>
2025-08-03 12:35:13 +0900

    chore: repository 수정

--- src/main/java/project/airbnb/clone/repository/AccommodationRepository.java
+++ src/main/java/project/airbnb/clone/repository/AccommodationRepository.java
@@ -2,6 +2,8 @@ package project.airbnb.clone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccommodationRepository extends JpaRepository<T, ID> {
+import project.airbnb.clone.entity.Accommodation;
+
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 
 }
