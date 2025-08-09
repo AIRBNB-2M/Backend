bf7a1d8352745c4dfeabbbf035b7a1c31496a1d0
GeunYeong <jgy091411@gmail.com>
2025-08-09 16:07:55 +0900

    :sparkles: 카카오 인증 로직

---
+++ src/main/java/project/airbnb/clone/common/converters/impls/OAuth2KakaoProviderUserConverter.java
@@ -0,0 +1,29 @@
+package project.airbnb.clone.common.converters.impls;
+
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import project.airbnb.clone.common.converters.ProviderUserConverter;
+import project.airbnb.clone.common.converters.ProviderUserRequest;
+import project.airbnb.clone.consts.SocialType;
+import project.airbnb.clone.model.OAuthUtils;
+import project.airbnb.clone.model.ProviderUser;
+import project.airbnb.clone.model.social.KakaoUser;
+
+public class OAuth2KakaoProviderUserConverter implements ProviderUserConverter<ProviderUserRequest, ProviderUser> {
+
+    @Override
+    public ProviderUser converter(ProviderUserRequest providerUserRequest) {
+        if (!providerUserRequest.clientRegistration().getRegistrationId().equals(SocialType.KAKAO.getSocialName())) {
+            return null;
+        }
+
+        if (providerUserRequest.oAuth2User() instanceof OidcUser) {
+            return null;
+        }
+
+        return new KakaoUser(
+                OAuthUtils.getOtherAttributes(providerUserRequest.oAuth2User(), "kakao_account", "profile"),
+                providerUserRequest.oAuth2User(),
+                providerUserRequest.clientRegistration()
+        );
+    }
+}
