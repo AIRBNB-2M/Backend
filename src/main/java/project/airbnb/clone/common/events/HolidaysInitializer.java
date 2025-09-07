9646bb5e6ea50cec869087c3f9840803b92d2d7b
GeunYeong <jgy091411@gmail.com>
2025-09-07 15:09:31 +0900

    :sparkles: feat : 서버 시작 시 공휴일 레디스 저장

---
+++ src/main/java/project/airbnb/clone/common/events/HolidaysInitializer.java
@@ -0,0 +1,37 @@
+package project.airbnb.clone.common.events;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import project.airbnb.clone.common.clients.HolidayApiClient;
+import project.airbnb.clone.repository.redis.RedisRepository;
+import project.airbnb.clone.service.tour.HttpClientTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class HolidaysInitializer implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final HttpClientTemplate<HolidayApiClient> clientTemplate;
+    private final RedisRepository redisRepository;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        int year = LocalDate.now().getYear();
+        String key = "holidays:" + year;
+
+        if (redisRepository.hasKey(key)) {
+            return;
+        }
+
+        List<Map<String, String>> items = clientTemplate.fetchItems(client -> client.getHolidays(year));
+        List<String> holidays = items.stream()
+                                     .map(map -> map.get("locdate"))
+                                     .toList();
+        redisRepository.addSet(key, holidays);
+    }
+}
