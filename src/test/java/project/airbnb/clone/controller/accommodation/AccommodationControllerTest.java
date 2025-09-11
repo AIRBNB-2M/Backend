7cf8fb34bac4cbbb6b3b71257c515f4edf3bb122
GeunYeong <jgy091411@gmail.com>
2025-09-11 16:10:14 +0900

    :white_check_mark: test : 숙소 상세 조회 API 테스트 코드 및 문서화

--- src/test/java/project/airbnb/clone/controller/accommodation/AccommodationControllerTest.java
+++ src/test/java/project/airbnb/clone/controller/accommodation/AccommodationControllerTest.java
@@ -6,12 +6,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import project.airbnb.clone.controller.RestDocsTestSupport;
 import project.airbnb.clone.dto.PageResponseDto;
+import project.airbnb.clone.dto.accommodation.DetailAccommodationResDto;
+import project.airbnb.clone.dto.accommodation.DetailAccommodationResDto.DetailImageDto;
+import project.airbnb.clone.dto.accommodation.DetailAccommodationResDto.DetailReviewDto;
 import project.airbnb.clone.dto.accommodation.FilteredAccListResDto;
 import project.airbnb.clone.dto.accommodation.MainAccListResDto;
 import project.airbnb.clone.dto.accommodation.MainAccResDto;
 import project.airbnb.clone.service.AccommodationService;
 import project.airbnb.clone.service.jwt.TokenService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -24,6 +28,7 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -196,6 +201,142 @@ class AccommodationControllerTest extends RestDocsTestSupport {
                                fieldWithPath("contents[].likedMe")
                                        .attributes(field("path", "likedMe"))
                                        .description("좋아요 여부")
+                       )
+               ));
+    }
+
+    @Test
+    @DisplayName("특정 숙소 상세 조회")
+    void getAccommodation() throws Exception {
+        //given
+        Long accommodationId = 1L;
+        DetailImageDto detailImageDto = new DetailImageDto(
+                "https://example.com/thumbnail.jpg",
+                List.of("https://example.com/a.jpg", "https://example.com/b.jpg")
+        );
+        List<String> amenities = List.of("바비큐장", "뷰티시설", "식음료장", "자전거대여");
+
+        LocalDateTime now = LocalDateTime.now();
+        List<DetailReviewDto> reviewDtos = List.of(
+                new DetailReviewDto(1L, "guest-A", "https://example.com/profile-A.jpg", now, now, 4.5, "review-content-1"),
+                new DetailReviewDto(2L, "guest-B", "https://example.com/profile-B.jpg", now, now, 4.7, "review-content-2")
+        );
+        DetailAccommodationResDto response = new DetailAccommodationResDto(accommodationId, "acc-title", 5, "경기도 부천시...", 35.3, 40.1,
+                "10:00", "14:00", "acc-overview", "054-855-8552", "7일 이내 100%",
+                55000, true, 4.8, detailImageDto, amenities, reviewDtos
+        );
+
+        given(accommodationService.getDetailAccommodation(any(), any())).willReturn(response);
+
+        //when
+        //then
+        mockMvc.perform(get("/api/accommodations/{id}", accommodationId))
+               .andExpectAll(
+                       status().isOk(),
+                       jsonPath("$.accommodationId").value(response.accommodationId()),
+                       jsonPath("$.title").value(response.title()),
+                       jsonPath("$.maxPeople").value(response.maxPeople()),
+                       jsonPath("$.address").value(response.address()),
+                       jsonPath("$.mapX").value(response.mapX()),
+                       jsonPath("$.mapY").value(response.mapY()),
+                       jsonPath("$.checkIn").value(response.checkIn()),
+                       jsonPath("$.checkOut").value(response.checkOut()),
+                       jsonPath("$.description").value(response.description()),
+                       jsonPath("$.number").value(response.number()),
+                       jsonPath("$.refundRegulation").value(response.refundRegulation()),
+                       jsonPath("$.price").value(response.price()),
+                       jsonPath("$.likedMe").value(response.likedMe()),
+                       jsonPath("$.avgRate").value(response.avgRate()),
+                       jsonPath("$.images.thumbnail").value(detailImageDto.thumbnail()),
+                       jsonPath("$.images.others.length()").value(detailImageDto.others().size()),
+                       jsonPath("$.amenities.length()").value(amenities.size()),
+                       jsonPath("$.reviews.length()").value(reviewDtos.size())
+               )
+               .andDo(restDocs.document(
+                       pathParameters(
+                               parameterWithName("id").description("검색 숙소 ID")
+                       ),
+                       requestHeaders(
+                               headerWithName(AUTHORIZATION).optional().description("Bearer {액세스 토큰}")
+                       ),
+                       responseFields(
+                               fieldWithPath("accommodationId")
+                                       .attributes(field("path", "accommodationId"))
+                                       .description("숙소 ID"),
+                               fieldWithPath("title")
+                                       .attributes(field("path", "title"))
+                                       .description("숙소 제목"),
+                               fieldWithPath("maxPeople")
+                                       .optional()
+                                       .attributes(field("path", "maxPeople"))
+                                       .description("숙소 최대 수용 인원"),
+                               fieldWithPath("address")
+                                       .attributes(field("path", "address"))
+                                       .description("숙소 주소"),
+                               fieldWithPath("mapX")
+                                       .attributes(field("path", "mapX"))
+                                       .description("숙소 X 좌표 값"),
+                               fieldWithPath("mapY")
+                                       .attributes(field("path", "mapY"))
+                                       .description("숙소 Y 좌표 값"),
+                               fieldWithPath("checkIn")
+                                       .optional()
+                                       .attributes(field("path", "checkIn"))
+                                       .description("숙소 체크인 시간"),
+                               fieldWithPath("checkOut")
+                                       .optional()
+                                       .attributes(field("path", "checkOut"))
+                                       .description("숙소 체크아웃 시간"),
+                               fieldWithPath("description")
+                                       .optional()
+                                       .attributes(field("path", "description"))
+                                       .description("숙소 상세 설명"),
+                               fieldWithPath("number")
+                                       .optional()
+                                       .attributes(field("path", "number"))
+                                       .description("숙소 번호"),
+                               fieldWithPath("refundRegulation")
+                                       .optional()
+                                       .attributes(field("path", "refundRegulation"))
+                                       .description("숙소 환불 규정"),
+                               fieldWithPath("price")
+                                       .attributes(field("path", "price"))
+                                       .description("숙소 가격"),
+                               fieldWithPath("likedMe")
+                                       .attributes(field("path", "likedMe"))
+                                       .description("좋아요 여부"),
+                               fieldWithPath("avgRate")
+                                       .attributes(field("path", "avgRate"))
+                                       .description("평균 평점"),
+                               fieldWithPath("images.thumbnail")
+                                       .attributes(field("path", "thumbnail"))
+                                       .description("숙소 썸네일 이미지"),
+                               fieldWithPath("images.others")
+                                       .attributes(field("path", "others"))
+                                       .description("숙소 썸네일 외 모든 이미지 목록"),
+                               fieldWithPath("amenities")
+                                       .description("숙소 보유 편의시설 목록"),
+                               fieldWithPath("reviews[].guestId")
+                                       .attributes(field("path", "guestId"))
+                                       .description("리뷰 작성자 ID"),
+                               fieldWithPath("reviews[].guestName")
+                                       .attributes(field("path", "guestName"))
+                                       .description("리뷰 작성자명"),
+                               fieldWithPath("reviews[].profileUrl")
+                                       .attributes(field("path", "profileUrl"))
+                                       .description("리뷰 작성자 프로필 이미지"),
+                               fieldWithPath("reviews[].guestCreatedDate")
+                                       .attributes(field("path", "guestCreatedDate"))
+                                       .description("리뷰 작성자 가입일"),
+                               fieldWithPath("reviews[].reviewCreatedDate")
+                                       .attributes(field("path", "reviewCreatedDate"))
+                                       .description("리뷰 작성일"),
+                               fieldWithPath("reviews[].rating")
+                                       .attributes(field("path", "rating"))
+                                       .description("리뷰 평점"),
+                               fieldWithPath("reviews[].content")
+                                       .attributes(field("path", "content"))
+                                       .description("리뷰 내용")
                        )
                ));
     }
