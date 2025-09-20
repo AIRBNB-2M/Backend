ff8873407eeb4ebb3bea94b5145cc65ff8c098e8
GeunYeong <jgy091411@gmail.com>
2025-09-20 21:54:04 +0900

    :card_file_box: feat : 숙소 조회 이력 엔티티 추가

---
+++ src/main/java/project/airbnb/clone/entity/ViewHistory.java
@@ -0,0 +1,46 @@
+package project.airbnb.clone.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "view_histories", uniqueConstraints =
+    @UniqueConstraint(name = "uk_view_histories_guest_accommodation", columnNames = {"guest_id", "accommodation_id"})
+)
+public class ViewHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "view_history_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accommodation_id", nullable = false)
+    private Accommodation accommodation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "guest_id", nullable = false)
+    private Guest guest;
+
+    @Column(name = "viewed_at", nullable = false)
+    private LocalDateTime viewedAt;
+}
