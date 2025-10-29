4870fd4370bbd87e69f3351da5192d6da985a295
GeunYeong <jgy091411@gmail.com>
2025-10-29 19:56:46 +0900

    :bug: fix : Redis&Jpa 레포지토리 패키지 명시

--- src/main/java/project/airbnb/clone/config/infra/RedisConfig.java
+++ src/main/java/project/airbnb/clone/config/infra/RedisConfig.java
@@ -1,19 +1,15 @@
 package project.airbnb.clone.config.infra;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories(basePackages = "project.airbnb.clone.repository.redis")
 public class RedisConfig {
 
     @Bean
@@ -26,23 +22,5 @@ public class RedisConfig {
         configuration.setPassword(password);
 
         return new LettuceConnectionFactory(configuration);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(RedisSerializer.string());
-        redisTemplate.setValueSerializer(serializer);
-        redisTemplate.setHashKeySerializer(RedisSerializer.string());
-        redisTemplate.setHashValueSerializer(serializer);
-
-        return redisTemplate;
     }
 }
--- src/main/java/project/airbnb/clone/config/jpa/JpaConfig.java
+++ src/main/java/project/airbnb/clone/config/jpa/JpaConfig.java
@@ -2,8 +2,10 @@ package project.airbnb.clone.config.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "project.airbnb.clone.repository.jpa")
 public class JpaConfig {
 }
