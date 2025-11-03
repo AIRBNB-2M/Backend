879fb7d92f72140e8b6c510f50d2b75299911a44
GeunYeong <jgy091411@gmail.com>
2025-11-03 09:22:35 +0900

    :recycle: chore : ChatRoomQueryRepository 코드 정리

--- src/main/java/project/airbnb/clone/repository/query/ChatRoomQueryRepository.java
+++ src/main/java/project/airbnb/clone/repository/query/ChatRoomQueryRepository.java
@@ -17,8 +17,6 @@ import project.airbnb.clone.entity.chat.QChatRoom;
 import java.util.List;
 import java.util.Optional;
 
-import static project.airbnb.clone.entity.chat.QChatParticipant.chatParticipant;
-
 @Repository
 @RequiredArgsConstructor
 public class ChatRoomQueryRepository {
@@ -32,37 +30,46 @@ public class ChatRoomQueryRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public boolean existsByTwoGuestIds(Long guestId1, Long guestId2) {
-        QChatParticipant cp1 = chatParticipant;
-        QChatParticipant cp2 = new QChatParticipant("cp2");
+    public Optional<ChatRoomResDto> findChatRoomInfo(Long currentGuestId, Long otherGuestId, ChatRoom chatRoom) {
+        BooleanExpression currentUserCond = CP1.chatRoom.eq(CR).and(CP1.guest.id.eq(currentGuestId));
+        BooleanExpression otherUserCond = CP2.chatRoom.eq(CR).and(CP2.guest.id.eq(otherGuestId));
 
-        return queryFactory.selectOne()
-                           .from(cp1)
-                           .join(cp2).on(cp1.chatRoom.eq(cp2.chatRoom))
-                           .where(cp1.guest.id.eq(guestId1).and(cp2.guest.id.eq(guestId2)))
-                           .fetchFirst() != null;
-    }
-
-    public Optional<ChatRoomResDto> findChatRoomInfo(Long otherGuestId, Long creatorId, ChatRoom chatRoom) {
-        BooleanExpression otherGuestCond = CP2.chatRoom.eq(CR).and(CP2.guest.id.eq(otherGuestId));
-
-        return Optional.ofNullable(buildChatRoomQuery(creatorId, otherGuestCond)
-                .where(CR.eq(chatRoom))
-                .fetchOne()
+        return Optional.ofNullable(
+                queryFactory
+                        .select(Projections.constructor(ChatRoomResDto.class,
+                                CR.id,
+                                CP1.customRoomName,
+                                OTHER_GUEST.id,
+                                OTHER_GUEST.name,
+                                OTHER_GUEST.profileUrl,
+                                CP2.isActive,
+                                CM.content,
+                                CM.createdAt,
+                                buildUnreadCountSubQuery()
+                        ))
+                        .from(CR)
+                        .join(CP1).on(currentUserCond)
+                        .join(CP2).on(otherUserCond)
+                        .join(CP2.guest, OTHER_GUEST)
+                        .leftJoin(CM).on(CM.id.eq(
+                                JPAExpressions.select(SUB_CM.id.max())
+                                              .from(SUB_CM)
+                                              .where(SUB_CM.chatRoom.eq(CR))
+                        ))
+                        .where(CR.eq(chatRoom))
+                        .fetchOne()
         );
     }
 
     public List<ChatRoomResDto> findChatRooms(Long guestId) {
-        BooleanExpression otherGuestCond = CP2.chatRoom.eq(CR).and(CP2.guest.id.ne(guestId));
+        // CP1: 현재 사용자의 참가 정보
+        // CP2: 상대방의 참가 정보
+        BooleanExpression currentUserCond = CP1.chatRoom.eq(CR)
+                                                        .and(CP1.guest.id.eq(guestId))
+                                                        .and(CP1.isActive.isTrue());
+        BooleanExpression otherUserCond = CP2.chatRoom.eq(CR)
+                                                      .and(CP2.guest.id.ne(guestId));
 
-        return buildChatRoomQuery(guestId, otherGuestCond).fetch();
-    }
-
-    private JPQLQuery<ChatRoomResDto> buildChatRoomQuery(Long guestId, BooleanExpression otherGuestCond) {
-        JPQLQuery<Integer> unreadCount = JPAExpressions.select(SUB_CM.count().intValue())
-                                                        .from(SUB_CM)
-                                                        .where(SUB_CM.chatRoom.eq(CR).and(CP1.lastReadMessage.isNull()
-                                                                                                             .or(SUB_CM.id.gt(CP1.lastReadMessage.id))));
         return queryFactory
                 .select(Projections.constructor(ChatRoomResDto.class,
                         CR.id,
@@ -73,19 +80,29 @@ public class ChatRoomQueryRepository {
                         CP2.isActive,
                         CM.content,
                         CM.createdAt,
-                        unreadCount
+                        buildUnreadCountSubQuery()
                 ))
                 .from(CR)
-                .join(CP1).on(CP1.chatRoom.eq(CR),
-                        CP1.guest.id.eq(guestId),
-                        CP1.isActive.isTrue()
-                )
-                .join(CP2).on(otherGuestCond)
+                .join(CP1).on(currentUserCond)
+                .join(CP2).on(otherUserCond)
                 .join(CP2.guest, OTHER_GUEST)
                 .leftJoin(CM).on(CM.id.eq(
                         JPAExpressions.select(SUB_CM.id.max())
                                       .from(SUB_CM)
                                       .where(SUB_CM.chatRoom.eq(CR))
-                ));
+                ))
+                .orderBy(CM.createdAt.desc().nullsLast())
+                .fetch();
+    }
+
+    private JPQLQuery<Integer> buildUnreadCountSubQuery() {
+        return JPAExpressions
+                .select(SUB_CM.count().intValue())
+                .from(SUB_CM)
+                .where(SUB_CM.chatRoom.eq(CR)
+                                      .and(CP1.lastReadMessage.isNull().or(SUB_CM.id.gt(CP1.lastReadMessage.id)))
+                                      // 본인이 보낸 메시지 제외
+                                      .and(SUB_CM.writer.ne(CP1.guest))
+                );
     }
 }
